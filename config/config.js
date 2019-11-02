99644d0e751a142a9862be5a02cf51d12da3c76b
Zubeir Mohamed <zumo@solute.de>
2019-11-03 00:53:46 +0100

    run migration and config.js instead of json

--- config/config.js
+++ config/config.js
@@ -1,18 +1,20 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": process.env.PG_LOCAL_USERNAME,
+    "password": process.env.PG_LOCAL_PASSWORD,
+    "database": process.env.PG_LOCAL_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "dialect": "postgres",
+    "operatorsAliases": false,
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "postgres",
     "operatorsAliases": false
   },
   "production": {
@@ -20,7 +22,7 @@
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "postgres",
     "operatorsAliases": false
   }
 }
