505051ec328ed3bf092f718f1e516dd14e1feb75
Zubeir Mohamed <zumo@solute.de>
2019-11-10 22:07:59 +0100

    setup db

--- config/config.js
+++ config/config.js
@@ -9,17 +9,25 @@ module.exports = {
     "dialect": "postgres",
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    },
+    "use_env_variable": process.env.DB_URI
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    },
+    "use_env_variable": process.env.DB_URI
   }
 }
